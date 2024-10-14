b6d2b4f83a8cf6a60df9ea67c7be33c7847db3fe
huybang2017 <ndhbang2017@gmail.com>
2024-10-14 18:06:30 +0700

    fix: fix dto update user

--- back-end/src/users/dto/update-user.dto.ts
+++ back-end/src/users/dto/update-user.dto.ts
@@ -1,25 +1,4 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
-import { IsEmail, IsInt, IsOptional, IsString } from 'class-validator';
+import { PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  @ApiProperty({ description: 'Username of the user', required: false })
-  @IsOptional()
-  @IsString()
-  username?: string;
-
-  @ApiProperty({ description: 'Password of the user', required: false })
-  @IsOptional()
-  @IsString()
-  password?: string;
-
-  @ApiProperty({ description: 'Email of the user', required: false })
-  @IsOptional()
-  @IsEmail()
-  email?: string;
-
-  @ApiProperty({ description: 'Role ID of the user', required: false })
-  @IsOptional()
-  @IsInt()
-  role_id?: number;
-}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
