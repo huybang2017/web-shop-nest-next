ebc3120a6c201b2d00d369aa753583903f9fbc34
huybang2017 <ndhbang2017@gmail.com>
2024-10-19 12:49:23 +0700

    refactor: adjust structure match project

--- back-end/src/roles/dto/create-role.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty } from 'class-validator';
-
-export class CreateRoleDto {
-  @ApiProperty({ description: 'Name of the role', example: 'Admin' })
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-}
