10628ec608bcdb929a5cd07f09e20a1f614a543e
huybang2017 <ndhbang2017@gmail.com>
2024-10-22 01:41:57 +0700

    chore: add export index.ts

---
+++ back-end/src/dto/index.ts
@@ -0,0 +1,5 @@
+export * from './product/create-product.dto';
+export * from './product/update-product.dto';
+export * from './user/create-user.dto';
+export * from './user/update-user.dto';
+export * from './auth/sign-in.dto';
