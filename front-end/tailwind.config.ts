93327e5266d406a3a2fe083daf32a11a52e208d2
huybang2017 <ndhbang2017@gmail.com>
2024-10-22 19:55:42 +0700

    .

--- front-end/tailwind.config.ts
+++ front-end/tailwind.config.ts
@@ -1,25 +1,28 @@
-import { nextui } from "@nextui-org/react";
-import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/react'
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+      },
       screens: {
-        sx: "450px",
-        sm: "650px",
-        md: "878px",
-        lg: "1070px",
-        xl: "1200px",
+        sx: '450px',
+        sm: '650px',
+        md: '878px',
+        lg: '1070px',
+        xl: '1200px',
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [nextui()],
-};
-export default config;
+}
+export default config
