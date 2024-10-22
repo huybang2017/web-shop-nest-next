18f1c2a34dcf2842659227722a381a253ee341e5
huybang2017 <ndhbang2017@gmail.com>
2024-10-22 19:55:00 +0700

    fix: fix ui banner

--- front-end/src/components/banner/index.tsx
+++ front-end/src/components/banner/index.tsx
@@ -2,9 +2,9 @@
 
 export default function Banner() {
   return (
-    <div className="w-full h-2/4 bg-green-800 text-white p-4">
+    <div className="w-full h-full bg-black text-white p-4 rounded-md">
       <div className="grid grid-cols-2 px-2">
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center">
           <div>
             <h1 className="text-4xl font-extrabold mb-5">
               Welcome to the Jungle
@@ -16,18 +16,18 @@ export default function Banner() {
               nec scelerisque lacinia
             </p>
             <div className="grid grid-cols-2 gap-4">
-              <button className="w-36 h-12 rounded-full bg-green-500 hover:bg-green-600 transition duration-200 ease-in-out dark:bg-green-600 dark:hover:bg-green-700">
+              <button className="w-36 h-12 rounded-full bg-black hover:bg-gray-800 transition duration-200 ease-in-out text-white">
                 Shop Now
               </button>
-              <button className="border border-white rounded-full w-36 h-12 transition duration-200 ease-in-out hover:bg-white hover:text-black dark:border-gray-700 dark:hover:bg-gray-700 dark:hover:text-white">
+              <button className="border border-white rounded-full w-36 h-12 transition duration-200 ease-in-out hover:bg-white hover:text-black">
                 Explore
               </button>
             </div>
           </div>
         </div>
-        <div>
+        <div className="w-[300px] h-[300px]">
           <img
-            className="w-auto h-auto object-center"
+            className="w-full h-full object-center"
             src="/images/banner.png"
             alt="banners"
           />
