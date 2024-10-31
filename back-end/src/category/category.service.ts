77711086e7007b5750d74b1ec4483b0e57ed4074
huybang2017 <ndhbang2017@gmail.com>
2024-10-31 23:37:41 +0700

    chore: update service with query sort, pagination, filter

--- back-end/src/category/category.service.ts
+++ back-end/src/category/category.service.ts
@@ -2,8 +2,9 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProductCategory } from 'src/entities/product-category.entity';
 import { Repository } from 'typeorm';
-import { UpdateProductCategoryDto } from './dto/update-category.dto'; // Fix the import path
+import { UpdateProductCategoryDto } from './dto/update-category.dto';
 import { CreateProductCategoryDto } from './dto/create-catefory.dto';
+import { FilterDto, PaginationDto, SortDto } from './dto/category-params.dto';
 
 @Injectable()
 export class CategoryService {
@@ -12,18 +13,46 @@ export class CategoryService {
     private readonly categoryRepository: Repository<ProductCategory>,
   ) {}
 
-  async findAll(): Promise<ProductCategory[]> {
-    const categories = await this.categoryRepository.find({
-      relations: ['parent_category_id'],
-    });
-    console.log(categories);
-    return categories;
+  async findAll(
+    filterDto: FilterDto,
+    sortDto: SortDto,
+    paginationDto: PaginationDto,
+  ): Promise<ProductCategory[]> {
+    const { category_name, parent_category_id } = filterDto;
+    const { sortField, sortOrder } = sortDto;
+    const { page, limit } = paginationDto;
+
+    const query = this.categoryRepository
+      .createQueryBuilder('category')
+      .leftJoinAndSelect('category.parent_category_id', 'parent_category_id')
+      .leftJoinAndSelect('category.promotionCategories', 'promotionCategories');
+
+    if (category_name) {
+      query.andWhere('category.category_name = :category_name', {
+        category_name,
+      });
+    }
+
+    if (parent_category_id) {
+      query.andWhere('category.parent_category_id = :parent_category_id', {
+        parent_category_id,
+      });
+    }
+
+    if (sortField && sortOrder) {
+      query.orderBy(`category.${sortField}`, sortOrder);
+    }
+    if (page && limit) {
+      query.skip((page - 1) * limit).take(limit);
+    }
+
+    return query.getMany();
   }
 
   async findOne(id: number): Promise<ProductCategory> {
     const category = await this.categoryRepository.findOne({
       where: { id },
-      relations: ['parent_category_id'],
+      relations: ['parent_category_id', 'promotionCategories'],
     });
     if (!category) {
       throw new NotFoundException(`Category with ID ${id} not found`);
