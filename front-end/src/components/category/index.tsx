2f8a7bae7a352f9ddfbb24bb47548d821802fe8c
huybang2017 <ndhbang2017@gmail.com>
2024-10-31 23:45:55 +0700

    chore: handle data category with react query

--- front-end/src/components/category/index.tsx
+++ front-end/src/components/category/index.tsx
@@ -1,54 +1,96 @@
 'use client'
+import { GrLinkNext, GrLinkPrevious } from 'react-icons/gr'
+import CardCategory from '../card-category'
+import { useRef } from 'react'
+import { queryOptions, useSuspenseQuery } from '@tanstack/react-query'
+import { getProductCategory } from '@/lib/apis/endpoints/get-product-categories'
 
-import { Accordion, AccordionItem } from '@nextui-org/react'
+const Category = () => {
+  const { data: resProductCategories } = useSuspenseQuery(
+    queryOptions({
+      queryKey: [
+        'fetchProductsPromotion',
+        {
+          category_name: null,
+          parent_category_id: null,
+          sortField: null,
+          sortOrder: null,
+          page: null,
+          limit: null,
+        },
+      ],
+      queryFn: async () => {
+        return await getProductCategory({
+          category_name: null,
+          parent_category_id: null,
+          sortField: null,
+          sortOrder: null,
+          page: null,
+          limit: null,
+        })
+      },
+    }),
+  )
+  const scrollRef = useRef<HTMLDivElement>(null)
 
-export default function Category() {
-  const defaultContent =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-
-  const itemClasses = {
-    base: 'py-0 w-full',
-    title: 'font-normal text-medium',
-    trigger: 'py-0 pt-4 flex items-center',
-    indicator: 'text-medium',
-    content: 'text-small px-2',
+  const scrollLeft = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft -= 100
+    }
   }
-  return (
-    <div>
-      <ul className="space-y-4">
-        <Accordion showDivider={false} itemClasses={itemClasses}>
-          <AccordionItem
-            key="1"
-            aria-label="Accordion 1"
-            title="Woman's Fashion"
-          >
-            {defaultContent}
-          </AccordionItem>
-          <AccordionItem key="2" aria-label="Accordion 2" title="Men's Fashion">
-            {defaultContent}
-          </AccordionItem>
-        </Accordion>
 
-        {/* Other categories outside the Accordion */}
-        <li className="px-2">
-          <span className="cursor-pointer">Electronics</span>
-        </li>
-        <li className="px-2">
-          <span className="cursor-pointer">Home & Lifestyle</span>
-        </li>
-        <li className="px-2">
-          <span className="cursor-pointer">Medican</span>
-        </li>
-        <li className="px-2">
-          <span className="cursor-pointer">Sports</span>
-        </li>
-        <li className="px-2">
-          <span className="cursor-pointer">Baby's & Toy</span>
-        </li>
-        <li className="px-2">
-          <span className="cursor-pointer">Heathy & Beauty</span>
-        </li>
-      </ul>
-    </div>
+  const scrollRight = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft += 100
+    }
+  }
+
+  return (
+    <>
+      <div className="pb-36">
+        <div className="flex items-center">
+          <div className="w-4 h-8 bg-red-600 mr-2 rounded-sm"></div>
+          <h1 className="text-xl font-bold text-red-600">Categories</h1>
+        </div>
+        <div className="flex justify-between items-center my-5">
+          <div className="flex justify-between items-center gap-20">
+            <span className="font-bold text-4xl">Browse By Category</span>
+          </div>
+          <div className="flex gap-4">
+            <button
+              onClick={scrollLeft}
+              className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-300 hover:bg-gray-200"
+            >
+              <GrLinkPrevious />
+            </button>
+            <button
+              onClick={scrollRight}
+              className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-300 hover:bg-gray-200"
+            >
+              <GrLinkNext />
+            </button>
+          </div>
+        </div>
+        <div
+          className="overflow-x-auto scrollbar-hide"
+          ref={scrollRef}
+          style={{ width: '100%', overflowX: 'auto' }}
+        >
+          <div className="flex gap-10 my-10" style={{ width: 'max-content' }}>
+            {resProductCategories?.data ? (
+              resProductCategories.data.map((category) => (
+                <div key={category.id} className="inline-block">
+                  <CardCategory category={category} />
+                </div>
+              ))
+            ) : (
+              <p>No categories available</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
+
+export default Category
