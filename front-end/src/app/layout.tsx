5f196e326efc96f2add98c5c0bdd7c4a2a492f0e
huybang2017 <ndhbang2017@gmail.com>
2024-10-31 23:50:04 +0700

    chore: update providers

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import '@fontsource/manrope'
 import { Providers } from './providers'
+import 'dotenv/config'
+import { ToastProvider } from '@/hook/useToast'
+import { UserProvider } from '@/hook/useDecodeToken'
 
 export default function RootLayout({
   children,
@@ -8,9 +11,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <UserProvider>
+            <ToastProvider>{children}</ToastProvider>
+          </UserProvider>
+        </Providers>
       </body>
     </html>
   )
--- front-end/src/app/providers.tsx
+++ front-end/src/app/providers.tsx
@@ -1,12 +1,19 @@
-import { NextUIProvider } from "@nextui-org/react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+'use client'
+
+import { NextUIProvider } from '@nextui-org/react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import React from 'react'
+import { getQueryClient } from './get-query-client'
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient()
   return (
     <NextUIProvider>
-      <NextThemesProvider attribute="class" defaultTheme="dark">
+      <QueryClientProvider client={queryClient}>
         {children}
-      </NextThemesProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </NextUIProvider>
-  );
+  )
 }
