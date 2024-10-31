fdf08f5e953edb44c52f761ef60c7f9f0bdbe7ef
huybang2017 <ndhbang2017@gmail.com>
2024-10-31 23:35:13 +0700

    chore: update controller auth with require and add curl post refresh token

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -1,30 +1,59 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiBody } from '@nestjs/swagger';
 import { SignInDto } from './dto/sign-in.dto';
 import { AuthService } from './auth.service';
+import { RefreshTokenDto } from './dto/refresh-token.dto';
 
 @ApiTags('Auth')
 @Controller('api/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @HttpCode(HttpStatus.OK)
-  @Post('login')
+  @Post('login-user')
   @ApiOperation({ summary: 'User login' })
   @ApiBody({
     description: 'User login credentials',
-    schema: {
-      type: 'object',
-      properties: {
-        email: { type: 'string', example: 'admin@example.com' },
-        password: { type: 'string', example: 'admin' },
-      },
-      required: ['email', 'password'],
-    },
+    type: SignInDto,
   })
-  @ApiResponse({ status: 200, description: 'Successful login' })
-  @ApiResponse({ status: 401, description: 'Invalid credentials' })
-  signIn(@Body() signInDto: SignInDto) {
-    return this.authService.signIn(signInDto.email, signInDto.password);
+  @UsePipes(new ValidationPipe({ transform: true }))
+  signInUser(@Body() signInDto: SignInDto) {
+    console.log(signInDto);
+    return this.authService.signInUser(signInDto.email, signInDto.password);
+  }
+
+  @Post('login-admin')
+  @ApiOperation({ summary: 'User login' })
+  @ApiBody({
+    description: 'User login credentials',
+    type: SignInDto,
+  })
+  @UsePipes(new ValidationPipe({ transform: true }))
+  signInAdmin(@Body() signInDto: SignInDto) {
+    console.log(signInDto);
+    return this.authService.signInAdmin(signInDto.email, signInDto.password);
+  }
+
+  @Post('refresh')
+  @ApiOperation({ summary: 'Refresh access and refresh tokens' })
+  @ApiBody({
+    type: RefreshTokenDto,
+    description: 'Provide the refresh token to get new tokens',
+  })
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async refreshTokens(@Body() body: RefreshTokenDto) {
+    const { refreshToken } = body;
+
+    if (!refreshToken) {
+      throw new UnauthorizedException('Refresh token is required');
+    }
+
+    return this.authService.refreshTokens(refreshToken);
   }
 }
